852d829043e1d8b496f1d7f232b4b12a476df8b8
shabbirislam <shabbirislam801@gmail.com>
2023-11-04 13:39:32 +0100

    - modified main.tf to work with app-runner - add in Dockerfile --platform

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
-FROM node:18 AS builder
+# Verwenden Sie ein Base-Image, das auf der gewünschten Architektur basiert
+FROM --platform=linux/amd64 node:18 AS builder
 WORKDIR /app
 COPY package.json ./
 RUN npm install
 COPY . .
 RUN npm run build
 
-FROM node:18 AS runner
+FROM --platform=linux/amd64 node:18 AS runner
 WORKDIR /app
 COPY --from=builder /app/next.config.js ./next.config.js
 COPY --from=builder /app/public ./public
--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -13,85 +13,49 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_ecs_cluster" "webApp" {
-  name = "webApp-cluster"
-}
 
-resource "aws_ecs_task_definition" "webApp" {
-  family                   = "webApp-family"
-  container_definitions    = <<DEFINITION
-[
-  {
-    "name": "webApp-container",
-    "image": "shabbirislam/devops-web-app:11",
-    "memory": 512,
-    "cpu": 256,
-    "essential": true,
-    "portMappings": [
+resource "aws_iam_role" "awsIAMRoleAppRunner" {
+  name = "awsIAMRoleAppRunner"
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
       {
-        "containerPort": 80,
-        "hostPort": 80
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "build.apprunner.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
       }
     ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "myrolespolicy" {
+  role       = aws_iam_role.awsIAMRoleAppRunner.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
+}
+
+resource "time_sleep" "waitrolecreate" {
+  depends_on       = [aws_iam_role.awsIAMRoleAppRunner]
+  create_duration  = "60s"
+}
+
+resource "aws_apprunner_service" "shabbir-app-runner" {
+  depends_on   = [time_sleep.waitrolecreate]
+  service_name = "shabbir-app-runner" # Replace with your desired service name
+  source_configuration {
+    authentication_configuration {
+      access_role_arn = aws_iam_role.awsIAMRoleAppRunner.arn
+    }
+    image_repository {
+      image_identifier      = "245154219216.dkr.ecr.us-east-1.amazonaws.com/shabbir-repo:latest" # Replace with your ECR repository URL and image tag
+      image_repository_type = "ECR"
+      image_configuration {
+        port = 80 # Replace with the desired port
+      }
+    }
   }
-]
-DEFINITION
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
-  cpu                      = "256"
-  memory                   = "512"
 }
-
-resource "aws_ecs_service" "webApp" {
-  name            = "webApp-service"
-  cluster         = aws_ecs_cluster.webApp.id
-  task_definition = aws_ecs_task_definition.webApp.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    subnets          = [aws_subnet.webApp.id]
-    security_groups  = [aws_security_group.webApp.id]
-    assign_public_ip = true
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.webApp.arn
-    container_name   = "webApp-container"
-    container_port   = 80
-  }
-}
-
-resource "aws_vpc" "webApp" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "webApp" {
-  vpc_id            = aws_vpc.webApp.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-}
-
-resource "aws_subnet" "webApp2" {
-  vpc_id            = aws_vpc.webApp.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
-}
-
-resource "aws_security_group" "webApp" {
-  vpc_id = aws_vpc.webApp.id
-}
-
-resource "aws_alb" "webApp" {
-  name            = "webApp-alb"
-  subnets         = [aws_subnet.webApp.id, aws_subnet.webApp2.id]
-  security_groups = [aws_security_group.webApp.id]
-}
-
-resource "aws_lb_target_group" "webApp" {
-  name        = "webApp-tg"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = aws_vpc.webApp.id
-  target_type = "ip"
+output "app_runner_url" {
+  value = aws_apprunner_service.shabbir-app-runner.service_url
 }
